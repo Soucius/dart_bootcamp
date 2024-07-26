98ee9ed8e066972e23e54d03ed822b3e58ff14ab
Soucius <soucius.ggwp@gmail.com>
2024-07-27 02:52:02 +0300

    string literals and interpolation

---
+++ section1/string_literals_interpolation.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  String name = "soucius";
+  String lastName = "kaenj";
+  var lesson = 'usage of dart\'s code';
+  String lessonDesc = "will learn dart and flutter";
+
+  print(name + " " + lastName);
+  print("$name $lastName");
+  print("$lastName length is: " + lastName.length.toString());
+  print("char length: ${lastName.length}");
+
+  double width = 10;
+  double height = 12;
+
+  print("width: $width and height: $height | area: ${width * height}");
+}
