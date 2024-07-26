5fd15cee37bbb9036b1606b7ececc3e895b791b3
Soucius <soucius.ggwp@gmail.com>
2024-07-27 02:23:55 +0300

    num, boolean and null safety

---
+++ section1/num_boolean.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  int age = 22;
+  print(age);
+
+  age = 44;
+  print(age);
+
+  num year = 2002;
+  print(year);
+
+  double number = 52;
+  print(number);
+
+  int weight = 70.2.toInt();
+  print(weight);
+
+  int? number2;
+  print(number2);
+}
