33ef7365f92dbd9c3efa1ef68432295f990fec7d
Soucius <soucius.ggwp@gmail.com>
2024-07-27 02:13:32 +0300

    data types and variables

---
+++ section1/data_types_and_variables.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+  int age = 22;
+  double average = 5.6;
+  num year = 2024;
+  String name = "soucius";
+  bool isMan = true;
+  bool isJob = false;
+  var school = "beykent university";
+  var schoolNum = 2002;
+  var isPet = true;
+
+  print(age);
+  print(average);
+  print(year);
+  print(name);
+  print(name + "'s fav colors");
+  print(isMan);
+  print(isJob);
+  print(school);
+  print(schoolNum);
+  print(isPet);
+}
