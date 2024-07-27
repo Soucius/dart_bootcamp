e4f5c95c211b1fa51f961188af5cb6b8dea43f89
Soucius <soucius.ggwp@gmail.com>
2024-07-27 13:05:54 +0300

    data from user and launch.json setting to collect data from terminal

---
+++ section2/data_from_user.dart
@@ -0,0 +1,14 @@
+import 'dart:ffi';
+import 'dart:io';
+
+void main(List<String> args) {
+  print("name: ");
+  String? name = stdin.readLineSync();
+  print("name: $name");
+
+  // ------------------------------------
+
+  print("age: ");
+  int? age = int.parse(stdin.readLineSync()!);
+  print("age: $age");
+}
