163d30d1d81f97d3c9e2fec4664795a198bceaae
Soucius <soucius.ggwp@gmail.com>
2024-08-01 02:18:41 +0300

    this usage

--- section5/contructors.dart
+++ section5/contructors.dart
@@ -1,6 +1,7 @@
 void main(List<String> args) {
   Car honda = Car(2020, "honda", true);
   honda.showInfos();
+  honda.calculateAge();
 }
 
 class Car {
@@ -8,12 +9,24 @@ class Car {
   String? brand;
   bool? isAutomatic;
 
-  Car(int modelYear, String brand, bool isAutomatic) {
+  Car(this.modelYear, this.brand, this.isAutomatic) {
     print("constructor worked");
   }
+
+  // Car(int modelYear, String brand, bool isAutomatic) {
+  //   print("constructor worked");
+
+  // this.modelYear = modelYear;
+  // this.brand = brand;
+  // this.isAutomatic = isAutomatic;
+  // }
 
   void showInfos() {
     print(
         "car model year: $modelYear, brand: $brand, is automatic: $isAutomatic");
+  }
+
+  void calculateAge() {
+    print("car age: ${DateTime.now().year - modelYear!}");
   }
 }
