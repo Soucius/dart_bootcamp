3d2e269d4785a82e993d92f6e73988f5262d6e0f
Soucius <soucius.ggwp@gmail.com>
2024-08-05 00:35:40 +0300

    immutable class

---
+++ section7/immutable_class.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  const Student soucius = Student(5, "soucius");
+}
+
+class Student {
+  final int id;
+  final String name;
+
+  const Student(this.id, this.name);
+}
